e8cec1667655570065f039f695eec86f6ce5bfd4
Leonam Rabelo <rabeloleonam@gmail.com>
2025-02-06 16:42:08 -0300

    First Commit

---
+++ ComunicacaoSerial.c
@@ -0,0 +1,167 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/i2c.h"
+#include "hardware/pio.h"
+#include "hardware/clocks.h"
+//#include "hardware/uart.h"
+#include "ws2812.pio.h"
+#include "inc/ssd1306.h"
+#include "inc/font.h"
+
+// // UART defines
+// // By default the stdout UART is `uart0`, so we will use the second one
+// #define UART_ID uart1
+// #define BAUD_RATE 115200
+// // Use pins 4 and 5 for UART1
+// // Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
+// #define UART_TX_PIN 4
+// #define UART_RX_PIN 5
+#define I2C_PORT i2c1
+#define I2C_SDA 14
+#define I2C_SCL 15
+#define endereco 0x3C
+
+#define IS_RGBW false
+#define NUM_PIXELS 25
+#define NUM_NUMBERS 10
+#define BOTAO_A 5
+#define BOTAO_B 6
+#define WS2812_PIN 7
+#define LED_PIN_RED 13
+#define LED_PIN_BLUE 12
+#define LED_PIN_GREEN 11
+
+static volatile uint32_t last_time = 0; // Armazena o tempo do último evento (em microssegundos)
+volatile uint numero = 0;//variavel para inicializar o numero com 0, vai ser alterada nas interrupções (volatile)
+
+// Variável global para armazenar a cor (Entre 0 e 255 para intensidade)
+uint8_t led_r = 0; // Intensidade do vermelho
+uint8_t led_g = 0; // Intensidade do verde
+uint8_t led_b = 10; // Intensidade do azul
+
+static inline void put_pixel(uint32_t pixel_grb){
+    pio_sm_put_blocking(pio0, 0, pixel_grb << 8u);
+}
+
+static inline uint32_t urgb_u32(uint8_t r, uint8_t g, uint8_t b){
+    return ((uint32_t)(r) << 8) | ((uint32_t)(g) << 16) | (uint32_t)(b);
+}
+
+bool led_numeros[NUM_NUMBERS][NUM_PIXELS] = {
+    //Número 0
+    {
+    0, 1, 1, 1, 0,      
+    0, 1, 0, 1, 0, 
+    0, 1, 0, 1, 0,   
+    0, 1, 0, 1, 0,  
+    0, 1, 1, 1, 0   
+    },
+
+    //Número 1
+    {0, 1, 1, 1, 0,      
+    0, 0, 1, 0, 0, 
+    0, 0, 1, 0, 0,    
+    0, 1, 1, 0, 0,  
+    0, 0, 1, 0, 0   
+    },
+
+    //Número 2
+    {0, 1, 1, 1, 0,      
+    0, 1, 0, 0, 0, 
+    0, 1, 1, 1, 0,    
+    0, 0, 0, 1, 0,
+    0, 1, 1, 1, 0   
+    },
+
+    //Número 3
+    {0, 1, 1, 1, 0,      
+    0, 0, 0, 1, 0, 
+    0, 1, 1, 1, 0,    
+    0, 0, 0, 1, 0,  
+    0, 1, 1, 1, 0   
+    },
+
+    //Número 4
+    {0, 1, 0, 0, 0,      
+    0, 0, 0, 1, 0, 
+    0, 1, 1, 1, 0,    
+    0, 1, 0, 1, 0,     
+    0, 1, 0, 1, 0   
+    },
+
+    //Número 5
+    {0, 1, 1, 1, 0,      
+    0, 0, 0, 1, 0, 
+    0, 1, 1, 1, 0,   
+    0, 1, 0, 0, 0,  
+    0, 1, 1, 1, 0   
+    },
+
+    //Número 6
+    {0, 1, 1, 1, 0,      
+    0, 1, 0, 1, 0, 
+    0, 1, 1, 1, 0,    
+    0, 1, 0, 0, 0,  
+    0, 1, 1, 1, 0   
+    },
+
+    //Número 7
+    {0, 1, 0, 0, 0,      
+    0, 0, 0, 1, 0,   
+    0, 1, 0, 0, 0,    
+    0, 0, 0, 1, 0,  
+    0, 1, 1, 1, 0  
+    },
+
+    //Número 8
+    {0, 1, 1, 1, 0,      
+    0, 1, 0, 1, 0, 
+    0, 1, 1, 1, 0,    
+    0, 1, 0, 1, 0,  
+    0, 1, 1, 1, 0   
+    },
+
+    //Número 9
+    {0, 1, 1, 1, 0,      
+    0, 0, 0, 1, 0, 
+    0, 1, 1, 1, 0,    
+    0, 1, 0, 1, 0,  
+    0, 1, 1, 1, 0   
+    }
+};
+
+void set_one_led(uint8_t r, uint8_t g, uint8_t b, int numero){
+    //Define a cor com base nos parâmetros fornecidos
+    uint32_t color = urgb_u32(r, g, b);
+
+    //Define todos os LEDs com a cor especificada
+    for(int i = 0; i < NUM_PIXELS; i++){
+        if(led_numeros[numero][i]){ //Chama a matriz de leds com base no numero passado
+            put_pixel(color); //Liga o LED com um no buffer
+        }else{
+            put_pixel(0);  //Desliga os LEDs com zero no buffer
+        }
+    }
+}
+
+int main(){
+    // I2C Initialisation. Using it at 400Khz.
+    i2c_init(I2C_PORT, 400 * 1000);
+
+    gpio_set_function(I2C_SDA, GPIO_FUNC_I2C); // Set the GPIO pin function to I2C
+    gpio_set_function(I2C_SCL, GPIO_FUNC_I2C); // Set the GPIO pin function to I2C
+    gpio_pull_up(I2C_SDA); // Pull up the data line
+    gpio_pull_up(I2C_SCL); // Pull up the clock line
+    ssd1306_t ssd; // Inicializa a estrutura do display
+    ssd1306_init(&ssd, WIDTH, HEIGHT, false, endereco, I2C_PORT); // Inicializa o display
+    ssd1306_config(&ssd); // Configura o display
+    ssd1306_send_data(&ssd); // Envia os dados para o display
+
+    // Limpa o display. O display inicia com todos os pixels apagados.
+    ssd1306_fill(&ssd, false);
+    ssd1306_send_data(&ssd);
+
+    while(true){
+
+    }
+}
